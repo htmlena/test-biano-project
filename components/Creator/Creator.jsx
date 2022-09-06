1e68efb827819d6b18b667c2d0997106fa71e4d0
Lenka Shynkarova <lllee@post.cz>
2022-09-06 15:43:08 +0200

    status circle

--- components/Creator/Creator.jsx
+++ components/Creator/Creator.jsx
@@ -10,15 +10,17 @@ const Creator = () => {
         </div>
         <div className="max-w-6xl my-0 mx-auto pt-4">
           <div className="flex flex-col md:flex-row md:justify-around border border-orange rounded-lg bg-white">
-            <div className="flex flex-row p-4 items-center">
+            <div className="flex flex-row p-6 md:py-8 items-center">
               <div>
-                <Image
-                  className="rounded-full"
-                  src={avatar}
-                  alt="avatar"
-                  width={72}
-                  height={72}
-                />
+                <div className="border-8 border-violet rounded-full p-1 w-24 h-24 flex justify-center items-center">
+                  <Image
+                    className="rounded-full"
+                    src={avatar}
+                    alt="avatar"
+                    width={80}
+                    height={80}
+                  />
+                </div>
               </div>
               <div className="pl-6">
                 <p className="font-semibold text-base md:text-2xl">privet</p>
@@ -29,21 +31,21 @@ const Creator = () => {
               </div>
             </div>
             <div className="flex flex-row justify-center border-y md:border-y-0 md:border-x border-yellow">
-              <div className="p-4 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3">
+              <div className="p-6 md:py-8 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3">
                 <p className="text-gray">Body</p>
                 <p className="text-violet font-medium">375 b.</p>
               </div>
-              <div className="p-4 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3 border-x border-yellow">
+              <div className="p-6 md:py-8 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3 border-x border-yellow">
                 <p className="text-gray">Projekty</p>
                 <p className="text-violet font-medium">3</p>
               </div>
-              <div className="p-4 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3">
+              <div className="p-6 md:py-8 flex flex-col justify-around items-center text-xs md:text-sm basis-1/3">
                 <p className="text-gray">Přátelé</p>
                 <p className="text-violet font-medium">5</p>
               </div>
             </div>
             <div className="flex flex-row justify-center items-center">
-              <button className="bg-pink m-4 px-10 h-12 rounded-3xl text-white w-full md:min-w-44">
+              <button className="bg-pink font-semibold m-4 px-10 h-12 rounded-3xl text-white w-full md:min-w-44">
                 Sledovat uživatele
               </button>
             </div>
