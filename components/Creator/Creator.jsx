dc70f5d264c960ca4fe10d84fc1bbe7fbcebec36
Lenka Shynkarova <lllee@post.cz>
2022-09-06 10:51:36 +0200

    Creator component - rough structure

---
+++ components/Creator/Creator.jsx
@@ -0,0 +1,20 @@
+const Creator = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="flex flex-row">
+        <div>Foto</div>
+        <div>Description</div>
+      </div>
+      <div>
+        <div>Blok 1</div>
+        <div>Blok 2</div>
+        <div>Blok 3</div>
+      </div>
+      <div>
+        <div>Button</div>
+      </div>
+    </div>
+  );
+};
+
+export default Creator;
